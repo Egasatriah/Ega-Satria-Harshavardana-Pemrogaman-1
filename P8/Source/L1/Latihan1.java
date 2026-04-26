7e67016f8b13a1f471164540241cc5ac6ea6995c
Student <student@example.com>
2026-04-27 00:45:23 +0700

    Add new workspace folders and settings

---
+++ P8/Source/L1/Latihan1.java
@@ -0,0 +1,12 @@
+package P8.Source.L1;
+
+public class Latihan1 {
+
+    public static void main(String[] args) {
+        int i;
+
+        for (i = 1; i <= 5; i++) {
+            System.out.println("Perulangan ke-" + i);
+        }
+    }
+}

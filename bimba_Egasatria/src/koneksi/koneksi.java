1aae142e251be3d8a3a91f5ae5c27362e9548be5
Ega Satria Harshavardana <114471189+Egasatriah@users.noreply.github.com>
2026-06-11 16:31:01 +0700

    Add files via upload

---
+++ bimba_Egasatria/src/koneksi/koneksi.java
@@ -0,0 +1,25 @@
+package koneksi;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import javax.swing.JOptionPane;
+
+public class koneksi {
+    
+    private static Connection koneksi;
+    
+    public static Connection getConnection() {
+        try {
+            String url = "jdbc:mysql://localhost:3306/db_bimba";
+            String user = "root";
+            String pass = "";
+            
+            Connection conn = DriverManager.getConnection(url, user, pass);
+            return conn;
+            
+        } catch (Exception e) {
+            System.out.println("Koneksi gagal: " + e.getMessage());
+            return null;
+        }
+    }
+}
